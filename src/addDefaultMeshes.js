5dd209a4b910c4432a72ba8be2b80ca42f1947ef
Jackie Elliott <je1283@nyu.edu>
2025-02-12 18:41:29 -0800

    added mesh physical

--- src/addDefaultMeshes.js
+++ src/addDefaultMeshes.js
@@ -3,7 +3,41 @@ import {
 	MeshBasicMaterial,
 	MeshStandardMaterial,
 	Mesh,
+	TextureLoader,
+	MeshPhysicalMaterial,
+	SphereGeometry,
 } from 'three'
+
+//define my texture loader
+const loader = new TextureLoader()
+
+//example function using textures and mesh physical material
+export const addTexturedMesh = () => {
+	color === map
+	//const color = loader.load('Ice_001_COLOR.jpg')
+	//const normal = loader.load('Ice_001_NRM.jpg')
+	//const displace = loader.load('Ice_001_DISP.png')
+	//const ambientOcclusion = loader.load('Ice_001_0CC.jpg')
+
+	const color = loader.load('Metal_007_basecolor.png')
+	const normal = loader.load('Metal_007_normal.png')
+	const displace = loader.load('Metal_007_height.png')
+	const ambientOcclusion = loader.load('Metal_007_ambientOcclusion.png')
+	const sphere = new SphereGeometry(0.75, 100, 100)
+	const sphereMaterial = new MeshPhysicalMaterial({
+		map: color,
+		normalMap: normal,
+		displacementMap: displace,
+		displacementScale: 0.3,
+		aoMap: ambientOcclusion,
+		metalness: 0.1,
+		roughness: 0,
+		transmission: 0.5,
+		ior: 2.33,
+	})
+	const sphereMesh = new Mesh(sphere, sphereMaterial)
+	return sphereMesh
+}
 
 export const addBoilerPlateMeshes = () => {
 	const box = new BoxGeometry(1, 1, 1)
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import './style.css'
 import * as THREE from 'three'
-import { addBoilerPlateMeshes, addStandardMesh } from './addDefaultMeshes'
+import {
+	addBoilerPlateMeshes,
+	addStandardMesh,
+	addTexturedMesh,
+} from './addDefaultMeshes'
 import { addLight } from './addDefaultLights'
 
 const renderer = new THREE.WebGLRenderer({ antialias: true })
+const clock = new THREE.Clock()
 
 //This is our default camera the perspective camera that imitates real life dimensions and proportions, just to recap the 4 parameters we pass in are: (Field of View, Aspect Ratio, Near Frustum, Far Frustum) aka (how wide can we see, what is the proportions of our screen, how close can our camera see, how far can our camera see)
 const camera = new THREE.PerspectiveCamera(
@@ -26,12 +31,15 @@ function init() {
 
 	meshes.default = addBoilerPlateMeshes()
 	meshes.standard = addStandardMesh()
+	meshes.physical = addTexturedMesh()
 
 	lights.default = addLight()
 
 	scene.add(lights.default)
 	scene.add(meshes.default)
 	scene.add(meshes.standard)
+	scene.add(meshes.physical)
+	console.log(meshes.physical.material.displacementScale)
 
 	camera.position.set(0, 0, 5)
 	resize()
@@ -47,8 +55,12 @@ function resize() {
 }
 
 function animate() {
+	const tick = clock.getElapsedTime()
 
 	requestAnimationFrame(animate)
+
+	meshes.physical.rotation.y += 0.01
+	meshes.physical.material.displacementScale = Math.sin(tick)
 
 	meshes.default.rotation.x += 0.01
 	meshes.default.rotation.y -= 0.01
